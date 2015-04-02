b4b65bc253fa87a132f651a48c9218a1aca8f9b2
Developer <developer@octopus.fit.fraunhofer.de>
2015-04-02 11:53:28 +0200

    Started cleaning up.

--- YUTableViewDemo/YUTableView/YUTableViewItem.h
+++ YUTableViewDemo/YUTableView/YUTableViewItem.h
@@ -8,42 +8,36 @@
 
 #import <Foundation/Foundation.h>
 
-/** Defines state of the cell. */
 typedef enum
 {
-    /** The item is not selected. */
-    YUTableViewItemStatusNormal,
-    /** The item is selected and doesn't have any subitem. */
+    YUTableViewItemStatusNotSelected,
     YUTableViewItemStatusSelected,
-    /** Item is selected, has subitems and these subitems are currently on table. */
     YUTableViewItemStatusSubmenuOpened,
     /** Subitem of subitem of this item is selected. Only used if "showAllItems" is NO */
-    YUTableViewItemStatusParent
+    YUTableViewItemStatusParent,
+    
+    YUTableViewItemStatusNormal __deprecated_msg("use YUTableViewItemStatusNotSelected instead.")
 } YUTableViewItemStatus;
 
-/** YUTableViewItem encapsulates information about cell.  */
 @interface YUTableViewItem : NSObject
 
-/** The current status of the item. */
 @property (nonatomic) YUTableViewItemStatus status;
-/** Subitems of item. All objects in "subItems" must be kind of "YUTableViewItem". */
+/** All objects in "subItems" must be kind of "YUTableViewItem". */
 @property (nonatomic, strong) NSArray * subItems;
 /** Custom data of item, which will be passed to cell view. */
 @property (nonatomic, strong) id itemData;
-/** The cell identifier of the cell which represents this item. If "cellIdentifier" is nil, "defaultCellIdentifier" in "YUTableView" will be used. */
+/** If nil, "defaultCellIdentifier" in "YUTableView" will be used. */
 @property (nonatomic, strong) NSString * cellIdentifier;
 
 /**
- Initializes an "YUTableViewItem" with custom data.
  @code
  YUTableViewItem * item = [[YUTableViewItem alloc] initWithData: @"Label"];
  @endcode
  @param data Custom data of the item, which will be passed to the cell view.
- @return The newly-initialized "YUTableViewItem"
  */
 - (id) initWithData: (id) data;
 /**
- Initializes an "YUTableViewItem" with given parameters. All parameters are optional.
+ All parameters are optional.
  @code
  YUTableViewItem * item, item2;
  YUTableViewItem * item3 = [[YUTableViewItem alloc] initWithSubitems: @[item, item2] data: @{@"key":@"value"} identifier: @"Cell"];
@@ -54,23 +48,19 @@ typedef enum
  */
 - (id) initWithSubitems: (NSArray *) items data: (id) data identifier: (NSString *) identifier;
 
-/**
- Returns parent of the item.
- @return Parent of the item.
- */
 - (YUTableViewItem *) parent;
 
-/** Creates a root item and sets parents of all items in array. "YUTableView" uses this method when creating cells. Using this method is NOT RECOMMENDED. */
-+ (NSArray *) initItemsFromArray: (NSArray *) items;
+/** Creates a root item and sets parents of all items in array. */
++ (NSArray *) setEmptyRootItemForItemList: (NSArray *) items;
 
-/** Changes active root item with given item. "YUTableView" calls this method. Using this method is NOT RECOMMENDED. */
-+ (void) setRootItem: (YUTableViewItem *) rootItem menuItems: (NSArray *) items;
++ (void) setRootItem: (YUTableViewItem *) rootItem toMenuItems: (NSArray *) items;
++ (void) setStatusesOfItemsToNotSelected: (NSArray *) items;
++ (void) setAllSubitemSatutesToNotSelected: (YUTableViewItem *) item;
 
-/** Sets statuses of all items in the array to normal. "YUTableView" calls this method. */
-+ (void) setStatusesOfItemsToNormal: (NSArray *) items;
-
-/** Sets statuses of item and all of its subitems to normal. "YUTableView" calls this method. */
-+ (void) setAllSubitemSatutesToNormal: (YUTableViewItem *) item;
-
+#pragma mark - deprecated methods
++ (NSArray *) initItemsFromArray: (NSArray *) items __deprecated_msg("use setEmptyRootItemForItemList: instead.");
++ (void) setRootItem: (YUTableViewItem *) rootItem menuItems: (NSArray *) items __deprecated_msg("use setRootItem:toMenuItems: instead.");
++ (void) setStatusesOfItemsToNormal: (NSArray *) items __deprecated_msg("use setStatusesOfItemsToNotSelected: instead.");
++ (void) setAllSubitemSatutesToNormal: (YUTableViewItem *) item __deprecated_msg("use setAllSubitemSatutesToNotSelected: instead.");;
 
 @end
